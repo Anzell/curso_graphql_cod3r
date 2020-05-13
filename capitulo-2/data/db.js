8cad34a85b7a8d8ad4d00089971c9744162fac33
Anzell <andrielferreira@outlook.com.br>
2020-05-13 18:23:47 -0300

    Enum

--- capitulo-2/data/db.js
+++ capitulo-2/data/db.js
@@ -5,21 +5,24 @@ const usuarios = [
         nome: "João silva",
         email: "joaoSilva@hotmail.com",
         idade: 29,
-        perfil_id:1
+        perfil_id:1,
+        status:"ATIVO"
     },
     {
         id: 2,
         nome: "Mateus sad",
         email: "mateus@hotmail.com",
         idade: 21,
-        perfil_id:2
+        perfil_id:2,
+        status:"INATIVO"
     },
     {
         id: 3,
         nome: "Ana",
         email: "ana@hotmail.com",
         idade: 23,
-        perfil_id:1
+        perfil_id:1,
+        status:"BLOQUEADO"
     }
 
 ];
