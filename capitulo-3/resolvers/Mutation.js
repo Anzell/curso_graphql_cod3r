6c07d6f5fd185117b6e9f6601e5618018c7654ff
Anzell <andrielferreira@outlook.com.br>
2020-05-13 18:44:17 -0300

    Validacao para evitar usuarios duplicados

--- capitulo-3/resolvers/Mutation.js
+++ capitulo-3/resolvers/Mutation.js
@@ -1,17 +1,20 @@
-const {usuarios,proximoId} = require("../data/db");
+const { usuarios, proximoId } = require("../data/db");
 
-module.exports={
-    novoUsuario(_,{nome,email,idade}){
-        const novo={
-            id:proximoId(),
-            nome,
-            email,
-            idade,
-            perfil_id:1,
-            status:"ATIVO"
-        };
-
-        usuarios.push(novo);
-        return novo;
+module.exports = {
+  //{nome,email,idade} ou então args
+  novoUsuario(_, args) {
+    const emailExistente = usuarios.some((u) => u.email === args.email);
+    if (emailExistente) {
+      throw new Error("E-mail já cadastrado");
     }
-}
+    const novo = {
+      id: proximoId(),
+      ...args,
+      perfil_id: 1,
+      status: "ATIVO",
+    };
+
+    usuarios.push(novo);
+    return novo;
+  },
+};
