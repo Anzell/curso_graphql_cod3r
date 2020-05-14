91f12141e736044161e70e106d187a0e76cd699d
Anzell <andrielferreira@outlook.com.br>
2020-05-14 20:51:30 -0300

    Alterando dados no banco de dados

---
+++ capitulo-4/testes/alterar.js
@@ -0,0 +1,35 @@
+const db=require("../config/db")
+
+const novoUsuario={
+    nome:"Pedro",
+    email:"pedro@empresa.com.br",
+    senha:"12345678"
+}
+
+async function exercicio(){
+    const {qtde} = await db("usuarios")
+        .count("* as qtde")
+        .first()
+
+    if(qtde===0){
+        await db("usuarios").insert(novoUsuario)
+    }
+
+    let { id } = await db("usuarios")
+        .select("id")
+        .limit(1)
+        .first()
+
+    await db("usuarios")
+        .where({id:id})
+        .update({
+            nome:"Pedro garcia"
+        })
+
+    return await db("usuarios")
+        .where({id:id})
+}
+
+exercicio()
+    .then(usuario=>console.log(usuario))
+    .finally(()=>db.destroy())
