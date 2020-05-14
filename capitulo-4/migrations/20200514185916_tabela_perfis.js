53f8b7cabe34eb33c746ead351e06722cd37243a
Anzell <andrielferreira@outlook.com.br>
2020-05-14 20:15:39 -0300

    Fazendo insercoes no banco

--- capitulo-4/migrations/20200514185916_tabela_perfis.js
+++ capitulo-4/migrations/20200514185916_tabela_perfis.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("perfis",table=>{
       table.increments("id").primary()
-      table.string("nome").notNull().unique
+      table.string("nome").notNull().unique()
       table.string("rotulo").notNull()
   }).then(function(){
     return knex("perfis").insert([
