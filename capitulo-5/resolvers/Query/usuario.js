9800559851f3c093f14eac9e410c3e828b9bbe71
Anzell <andrielferreira@outlook.com.br>
2020-05-19 13:11:44 -0300

    Login de Usuario. Com token

--- capitulo-5/resolvers/Query/usuario.js
+++ capitulo-5/resolvers/Query/usuario.js
@@ -1,6 +1,20 @@
 const db = require('../../config/db')
+const bcrypt  = require("bcrypt-nodejs")
+const {getUsuarioLogado}=require("../comum/usuario")
 
 module.exports = {
+    async login(_,{dados}){
+        const usuario = await db("usuarios").where({email:dados.email}).first();
+
+        if(!usuario){
+            return new Error("Usuario/Senha invalido")
+        }
+        const saoIguais = bcrypt.compareSync(dados.senha,usuario.senha)
+        if(!saoIguais){
+            return new Error("usuario/senha invalido")
+        }
+        return getUsuarioLogado(usuario);
+    },
     usuarios() {
         return db('usuarios')
     },
